98bf193f99db86e57600368ba6073d5ddadee4f2
MohammadAliAmir <37347364+MohammadAliAmir@users.noreply.github.com>
2018-10-09 09:41:41 +0200

    ali was hier

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-    System.out.println("Hello pi test wouter was hier!!!");
+    System.out.println("Hello pi test wouter was hier!!! ali was hier");
 
     }
 }
